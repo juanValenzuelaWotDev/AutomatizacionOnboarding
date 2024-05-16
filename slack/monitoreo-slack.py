b389031749896da46f179615c93dcfa788494c65
RonGT14 <ronald.dominguez.wot@gmail.com>
2024-05-16 09:43:10 -0600

    Update monitoreo-slack.py

--- slack/monitoreo-slack.py
+++ slack/monitoreo-slack.py
@@ -5,31 +5,33 @@ from slack_sdk.errors import SlackApiError
 import os
 
 # Configuración de Slack
-slack_token = os.environ.get("SLACK_BOT_TOKEN", "-")
+slack_token = os.environ.get("SLACK_BOT_TOKEN", "")
 client = WebClient(token=slack_token)
 
 # IDs de canal para las conversaciones directas de las tres personas
-person_1_channel_id = "D072UHCMTE0"
-person_2_channel_id = "D0736NJ44TD"
-person_3_channel_id = "D07392K6VGU"
+person_1_channel_id = os.environ.get("PERSON_1_CHANNEL_ID", "")
+person_2_channel_id = os.environ.get("PERSON_2_CHANNEL_ID", "")
+person_3_channel_id = os.environ.get("PERSON_3_CHANNEL_ID", "")
+
+# Lista de URLs a monitorear
+urls = ["https://thebotdev.com/", "https://repuestosus.com/", "https://wotdev.com/"]
 
 
-# Función para verificar el sitio web
 def verificar_sitio(url):
     try:
         response = requests.get(url, timeout=10)
         if response.status_code == 200:
-            message = (
+            print(
                 f"{time.strftime('%Y-%m-%d %H:%M:%S')} - El sitio {url} está activo.\n"
             )
         else:
             message = f"{time.strftime('%Y-%m-%d %H:%M:%S')} - El sitio {url} está inactivo. Código de estado: {response.status_code}\n"
+            enviar_mensaje_slack(message)
     except requests.RequestException as e:
         message = f"{time.strftime('%Y-%m-%d %H:%M:%S')} - No se pudo acceder al sitio {url}. Error: {e}\n"
-    return message
+        enviar_mensaje_slack(message)
 
 
-# Función para enviar mensaje a Slack
 def enviar_mensaje_slack(message):
     channel_ids = [person_1_channel_id, person_2_channel_id, person_3_channel_id]
     for channel_id in channel_ids:
@@ -40,13 +42,6 @@ def enviar_mensaje_slack(message):
             print(f"Error al enviar mensaje a Slack: {e.response['error']}")
 
 
-# URL del sitio que deseas monitorear
-url = "https://wotdev.com/"
-intervalo = 60 * 60  # Intervalo en segundos
-
-while True:
-    result = verificar_sitio(url)
-    with open("log_monitoreo.txt", "a") as file:
-        file.write(result)
-    enviar_mensaje_slack(result)
-    time.sleep(intervalo)
+# Monitorear los sitios una vez al iniciar el script
+for url in urls:
+    verificar_sitio(url)
